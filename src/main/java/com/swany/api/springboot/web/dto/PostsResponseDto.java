d08d6ae50f7f79e225a92dbd6fb45c33d20c3b93
jeonjeongchan <junjungchan@naver.com>
2020-05-21 20:16:47 +0900

    gitignore 등록

---
+++ src/main/java/com/swany/api/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.swany.api.springboot.web.dto;
+
+import com.swany.api.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+
+    }
+}
